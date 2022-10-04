d25f57e964514d512eb60cbabed26ea4e126fc35
Andres Villalobos <andres.villalobos@datagrove.com>
2022-10-03 18:08:33 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+require("./App.css");
+function App() {
+    var _a = (0, react_1.useState)(0), count = _a[0], setCount = _a[1];
+    return (<div className="App">
+
+    </div>);
+}
+exports.default = App;
+//# sourceMappingURL=App.js.map
